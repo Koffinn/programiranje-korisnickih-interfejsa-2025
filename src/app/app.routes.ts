f8bbca631adf4687065945aa17f77a0b379bb442
Mej Ivanović <maeivanovic8666@gmail.com>
2025-10-14 15:07:57 +0200

    Implemented search mechanics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Question } from './question/question';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About},
+    { path: 'question', component: Question },
+    { path: '**', redirectTo: '' }
+];
